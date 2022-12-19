b29478c4af554f2e915ea4996bdcef04d8114742
Matheus Carvalho <matheuscarvalho.pe@hotmail.com>
2022-12-19 00:41:22 -0300

    edição no nome do card

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function convertElementsPoke(pokemon){
     return `
     <div class="cartao-poke ${pokemon.type}">
         <span class="num-poke">#${pokemon.number}</span>
-        <span class="nome-poke">${pokemon.name.toUpperCase()}</span>
+        <span class="nome-poke">${pokemon.name}</span>
         <ol class="tipo">
             ${pokemon.types.map((type)=>`<li class="tipo-poke ${type}">${type}</li>`).join('')}
         </ol>
