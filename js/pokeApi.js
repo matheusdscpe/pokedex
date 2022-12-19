b66dc4c620fe733ef235fd3217a5d32c86bf3f2c
Matheus Carvalho <matheuscarvalho.pe@hotmail.com>
2022-12-19 01:19:46 -0300

    adição do btn para adicionar mas pokemons

--- js/pokeApi.js
+++ js/pokeApi.js
@@ -22,7 +22,7 @@ pokeApi.getpokemonDetails = (pokemon) => {
 
 
 
-pokeApi.getpokemons = (offset = 0, limit = 50) => {
+pokeApi.getpokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response)=> response.json())
